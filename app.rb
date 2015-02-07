ad169f60a99ae67f26ff6e9cb70f5b7bc288ebda
danielhex <danielhex@berkeley.edu>
2015-02-07 15:34:08 -0800

    try guess

--- app.rb
+++ app.rb
@@ -40,6 +40,12 @@ class HangpersonApp < Sinatra::Base
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    if !(letter =~ /[[:alpha:]]/) 
+      flash[:message] = 'Invalid guess.'
+    end
+    if letter in @game.wrong_guesses
+      flash[:message] = 'You have already used that letter.'
+    end
     redirect '/show'
   end
   
